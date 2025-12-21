3d0874663e9f8b614d8c7bdcdfce27638c316c9a
Prathap <prathap_hr@hotmail.com>
2025-12-21 11:49:04 -0800

    modified:   src/__pycache__/general_utility.cpython-313.pyc 	modified:   src/general_utility.py

--- src/general_utility.py
+++ src/general_utility.py
@@ -2,10 +2,7 @@ from torch.utils.data import DataLoader
 import tiktoken as tk
 from sanskrit_data_loader import GPTDatasetV1
 
-def create_dataloader_v1(txt, batch_size=4, max_length=256, 
-                         stride=128, shuffle=True, drop_last=True,
-                         num_workers=0):
-
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
     # Initialize the tokenizer
     tokenizer = tk.get_encoding("gpt2")
 
@@ -13,12 +10,6 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
 
     # Create dataloader
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        drop_last=drop_last,
-        num_workers=num_workers
-    )
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
 
     return dataloader
